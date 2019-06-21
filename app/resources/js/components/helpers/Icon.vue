558b7e99d922324b6d56fb93c4fd305b55a96407
Arthur Siqueira <asesilva@inf.ufpel.edu.br>
2019-06-21 11:26:07 -0300

    Icon helper component

---
+++ app/resources/js/components/helpers/Icon.vue
@@ -0,0 +1,14 @@
+<template>
+  <svg icon :class="className" :viewBox="viewBox || '0 0 24 24'">
+    <path :d="icon"></path>
+  </svg>
+</template>
+<script>
+export default {
+  props: ["className", "viewBox", "icon"],
+
+  mounted() {
+    console.log(this.icon);
+  }
+};
+</script>
