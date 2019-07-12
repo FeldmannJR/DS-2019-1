c098a01cb2d86f56544b57b95ed4e3d877e404df
FeldmannJR <feldmannjunior@gmail.com>
2019-07-12 13:50:40 -0300

    Mostrando indicadores

--- app/resources/views/showindicators.blade.php
+++ app/resources/views/showindicators.blade.php
@@ -3,14 +3,48 @@
 @push('scripts')
 @endpush
 @section('content')
-    <v-container fluid>
-        <v-layout>
+    <v-container>
+        <table>
+            <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Nome Indicador</th>
+                <th scope="col">Frequencia</th>
+                <th scope="col">Ultimo Valor</th>
+                <th scope="col">Ultimo Update</th>
+            </tr>
+            </thead>
+            @foreach($indicators as $indicador)
+                <tr>
+                    <th scope="row">{{$indicador->id}}</th>
+                    <td>{{$indicador->getCalculator() instanceof \App\Indicators\Calculators\Base\SpreadsheetCalculator ? ($indicador->getName() . " (planilha)") : $indicador->getName()}}</td>
+                    <td>{{$indicador->update_type->key}}</td>
+                    <td>
+                        @if($indicador->isPerUnit())
+                            <table class="table table-sm table-borderless">
+                                <tr>
+                                    <th scope="col">ID</th>
+                                    <th scope="col">Nome</th>
+                                    <th scope="col">Valor</th>
+                                </tr>
 
-            @foreach($indicators as $indicator)
-                <v-card class="pa-2" >
-                    {{$indicator->name}}
-                </v-card>
+                                @foreach($display_units as $unit)
+                                    <tr>
+                                        <td>{{$unit->id}}</td>
+                                        <td>{{$unit->name}}</td>
+                                        <td>{{$indicador->getDisplayLastValue($unit)}}</td>
+                                    </tr>
+                                @endforeach
+                            </table>
+                        @else
+                            {{$indicador->getDisplayLastValue()}}
+                        @endif
+                    </td>
+                    <td>{{$indicador->getDisplayLastUpdate()}}</td>
+                </tr>
+
             @endforeach
-        </v-layout>
+        </table>
+
     </v-container>
 @endsection
